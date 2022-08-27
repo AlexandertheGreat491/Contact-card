e8c8511dea323c8e2c857a828eba85219369771a
agvan <agvandyke25@gmail.com>
2022-08-27 13:26:32 -0500

    clean build created, hanged path for script in index.html in dist folder, updated webpack properties, updated index.js to import images & add images on load

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,17 @@
-import  "./form";
+// imports these files
+import "./form";
 import "./submit";
+
+/*allows the images to be included in the dependency graph*/
+
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
+
+// uses DOM manipulation to insert the images
+// add images on load
+window.addEventListener("load", function () {
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
