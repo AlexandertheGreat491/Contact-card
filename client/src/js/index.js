1805a067d298dc481b5dee64f71d7bfc8df732b0
agvan <agvandyke25@gmail.com>
2022-08-30 13:11:00 -0500

    created the ability to POST to the db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -7,6 +7,11 @@ import "../css/index.css";
 // my custom css
 import "../css/custom.css";
 
+// import Bootstrap's npm modules
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 /*allows the images to be included in the dependency graph*/
 
 import Logo from "../images/logo.png";
@@ -16,27 +21,59 @@ import Dog from "../images/dog.png";
 // uses DOM manipulation to insert the images
 // add images on load
 // imports the initDb function from database.js
-import { getDb, initdb, postDb } from './database';
+import { getDb, initdb, postDb } from "./database";
 
 // imports the fetchCards() function
-import {fetchCards} from './cards';
+import { fetchCards } from "./cards";
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
   initdb();
-
+  fetchCards();
   // temporarily placing the getDb() & postDb() function calls for the test data
   getDb();
   postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
   getDb();
-  
-  document.getElementById('logo').src = Logo;
-  document.getElementById('bearThumbnail').src = Bear;
-  document.getElementById('dogThumbnail').src = Dog;
+
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
 });
 
-// import Bootstrap's npm modules
-import { Tooltip, Toast, Popover } from "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { toggleForm, clearForm } from "./form";
 
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
 
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+ })
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
+});
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,40 +1,44 @@
 const path = require('path');
-// imports the html-webpack-plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-  module.exports = {
+
+module.exports = {
+    mode: 'development',
     entry: './src/js/index.js',
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
-      })
-    ],
+
     module: {
-      rules: [
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.n?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', {targets: "defaults"}]
-              ]
+        rules: [
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
             }
           }
-        }
+        ]
+      },
+
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './index.html',
+          title: 'Webpack Plugin',
+        })
       ]
-    }
-  };
+      
+};
