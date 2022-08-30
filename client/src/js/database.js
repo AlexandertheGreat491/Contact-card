891e8c01e319d4de4aea3ac754c2a11a0702f3db
agvan <agvandyke25@gmail.com>
2022-08-30 11:01:58 -0500

    added the getDb() function and several comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,3 +18,24 @@ export const initdb = async () => {
   });
 };
 
+// exports a function that is used to GET to the database
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  // creates a connection to the IndexedDB database & the version
+  const contactDb = await openDB("contact_db", 1);
+
+  // creates a new transaction & specifies the store & data privileges
+  const tx = contactDb.transaction('contacts', 'readonly');
+
+  // opens the desired object store
+  const store = tx.objectStore('contacts');
+
+  // uses the .getAll() method to get all data in the database
+  const request = store.getAll();
+
+  // gets the confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
